6c97094fdb9fc85de91dfb9d21e58bd481e10043
Sumit Gupta <sumitgupta7955@gmai.com>
2025-08-11 09:12:19 +0530

    feat: update socket connection to disable websocket transport

--- src/services/socket.js
+++ src/services/socket.js
@@ -4,12 +4,25 @@
 
 // export const socket = io(SOCKET_URL);
 
+// import { io } from "socket.io-client";
+
+// const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
+
+// export const socket = io(SOCKET_URL, {
+//   transports: ["websocket", "polling"],
+// });
+
+// socket.on("connect_error", (err) => {
+//   console.error("Socket connect error:", err.message);
+// });
+
+
 import { io } from "socket.io-client";
 
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL;
 
 export const socket = io(SOCKET_URL, {
-  transports: ["websocket", "polling"],
+  transports: ["polling"], // 🔹 disable websocket
 });
 
 socket.on("connect_error", (err) => {
